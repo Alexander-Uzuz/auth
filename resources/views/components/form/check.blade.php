b271a3e16c44cb193688f0322342a4b7b4933081
Alexander Yusiuz <uzuz98@mail.ru>
2024-02-25 15:17:26 +0200

    feat:registration store

--- resources/views/components/form/check.blade.php
+++ resources/views/components/form/check.blade.php
@@ -1,8 +1,12 @@
+@props(['name' => ''])
+
 <label class="form-check flex items-start">
-    <input type="checkbox" {{ $attributes }}
+    <input type="checkbox" name="{{ $name }}" value="1" {{ $attributes }}
         class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative top-1">
 
-    <span class="ml-3 block text-sm leading-6 text-gray-900">
+    <div class="ml-3 block text-sm leading-6 text-gray-900">
         {{ $slot }}
-    </span>
+
+    </div>
+
 </label>
