7eadd7168a6f493991b56bbca3fe684f6b4b7c47
Alexander Yusiuz <uzuz98@mail.ru>
2024-04-04 20:21:20 +0300

    password recovery (page)

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -2,6 +2,6 @@
 
 
 
-<a href="{{ $href }}" class="text-indigo-600 hover:text-indigo-500">
+<a href="{{ $href }}" {{ $attributes->class(['text-indigo-600 hover:text-indigo-500']) }}>
     {{ $slot }}
 </a>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\RegistrationController;
-use App\Http\Controllers\User\Settings\PasswordController;
-use App\Http\Controllers\User\Settings\ProfileController;
 use App\Http\Controllers\User\SettingsController;
+use App\Http\Controllers\User\Settings\ProfileController;
+use App\Http\Controllers\User\Settings\PasswordController as UserPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,12 @@ Route::middleware('guest')->group(function () {
 
     Route::view('/login', 'login.index')->name('login');
     Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+    Route::view('/password', 'password.index')->name('password');
+    Route::post('/password', [PasswordController::class, 'store'])->name('password.store');
+    Route::view('/password/confirm', 'password.confirm')->name('password.confirm');
+    Route::view('/password/{code}', 'password.edit')->name('password.edit');
+    Route::post('/password/{code}', [PasswordController::class, 'update'])->name('password.update');
 });
 
 Route::post('logout', [LogoutController::class, 'logout'])->name('logout');
@@ -37,6 +44,6 @@ Route::middleware(['auth', 'online'])->group(function () {
     Route::get('/user/settings', [SettingsController::class, 'index'])->name('user.settings');
     Route::get('/user/settings/profile', [ProfileController::class, 'edit'])->name('user.settings.profile.edit');
     Route::post('/user/settings/profile', [ProfileController::class, 'update'])->name('user.settings.profile.update');
-    Route::get('/user/settings/password', [PasswordController::class, 'edit'])->name('user.settings.password.edit');
-    Route::post('/user/settings/password', [PasswordController::class, 'update'])->name('user.settings.password.update');
+    Route::get('/user/settings/password', [UserPasswordController::class, 'edit'])->name('user.settings.password.edit');
+    Route::post('/user/settings/password', [UserPasswordController::class, 'update'])->name('user.settings.password.update');
 });
